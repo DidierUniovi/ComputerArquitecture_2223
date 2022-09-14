6ab9dafae73e93db3ed56f63fd12b15c4d5e2839
didierrc <didieryamil02@gmail.com>
2022-09-14 16:18:57 +0200

    Learning to use Printf

---
+++ session1-1/1-1output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 3;
+	float f = 2.4;
+	char * s = "Content";
+
+	printf("i content: %d\n", i);
+	printf("f content:  %f\n", f);
+	printf("s conttent: %s\n", s);
+
+	return 0;
+}
