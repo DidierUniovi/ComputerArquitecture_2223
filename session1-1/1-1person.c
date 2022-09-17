c623f546edde845ee067359042c800c5782e727c
didierrc <didieryamil02@gmail.com>
2022-09-17 21:39:41 +0200

    Pointers to Structs

--- session1-1/1-1person.c
+++ session1-1/1-1person.c
@@ -10,15 +10,23 @@ struct _Person
 
 // This abreviates the type name
 typedef struct _Person Person;
+typedef Person* PPerson;
 
 int main(int argc, char* argv[])
 {
 	Person Peter;
+
+	// Javier is a pointer to a Person Struct
+	PPerson pJavier;
+	pJavier = &Peter;
+
 	strcpy(Peter.name, "Peter");
 	Peter.heightcm = 175;
 	Peter.weightkg = 78.7;
 
-	printf("Peter's height: %d cm; Peter's weight: %f kg\n", 
+	pJavier->weightkg = 83.2;
+
+	printf("Peter's height: %d cm; Peter's weight: %.2f kg\n", 
 		Peter.heightcm, Peter.weightkg);
 
 	return 0;
